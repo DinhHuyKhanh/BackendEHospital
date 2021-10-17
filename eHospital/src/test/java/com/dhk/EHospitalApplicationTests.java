95ae42c41698e84b4a7e8370c906cb511b29865c
DinhHuyKhanh <khanhdinh141@gmail.com>
2021-10-17 15:00:54 +0700

    first commit

---
+++ eHospital/src/test/java/com/dhk/EHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
